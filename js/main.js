4294ac79a62347ff0c42f320540181889815e3f2
Jakub <jakubpachut@gmail.com>
2024-02-19 23:51:41 +0100

    finish footer

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSection = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
-	nav.classList.toggle('nav--active')
+	nav.classList.toggle('nav--active');
+
+	navBtnBars.classList.remove('black-bars-color');
+
+	if (!nav.classList.contains('nav--active')) {
+		handleObserver();
+	}
 
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
-			nav.classList.remove('nav--active')
+			nav.classList.remove('nav--active');
 		})
 	})
 
-	handleNavItemAnimation();
-    deleteAnimation();
+	handleNavItemAnimation()
+	deleteAnimation()
 }
 
 const handleNavItemAnimation = () => {
 	let delayTime = 0
 
 	allNavItems.forEach(item => {
-		item.classList.toggle('nav-item-animation');
-		item.style.animationDelay = '.' + delayTime + 's';
-        delayTime++;
+		item.classList.toggle('nav-item-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
 	})
 }
-
-
-navBtn.addEventListener('click', handleNav)
 
 const deleteAnimation = () => {
 	allNavItems.forEach(item => {
@@ -37,3 +43,24 @@ const deleteAnimation = () => {
 		})
 	})
 }
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSection.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
+navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
